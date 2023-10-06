9da89771cbcf3a3a2407cc6081725124cbfa4869
Preethi grace <81759829+Preethigrace-7@users.noreply.github.com>
2023-10-06 21:41:47 +0530

    Add files via upload

---
+++ main.v
@@ -0,0 +1,143 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:44:35 07/14/2023 
+// Design Name: 
+// Module Name:    main 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+// Code your design here
+module DigitalCalendar (
+  input wire clk,  
+  input wire reset, 
+  output wire [6:0] year,   // 7-bit output for year (0-99)
+  output wire [4:0] month,  // 4-bit output for month (1-12)
+  output wire [4:0] day     // 5-bit output for day (1-31)
+);
+
+  reg [6:0] count_year;   
+  reg [4:0] count_month;  
+  reg [4:0] count_day;    
+
+  always @(posedge clk or posedge reset) 
+	 begin//1
+    if (reset) 
+	 begin
+      count_year <= 7'd1;    // Initialize year counter to 1
+      count_month <= 5'd1;   // Initialize month counter to 1
+      count_day <= 5'd1;     // Initialize day counter to 1
+    end 
+	 else 
+		begin
+/*if(count_year/400==0 || (count_year/4==0 && count_year/100 !=0))
+begin
+if(count_month==2)
+			begin
+			if (count_day == 5'd29) 
+				begin
+				count_day <= 5'd1;   
+				if (count_month == 4'd12) 
+					begin
+					count_month <= 5'd1;   
+					count_year <= count_year + 7'd1;   
+					end 
+				else 
+					begin
+					count_month <= count_month + 5'd1;   
+					end
+            end
+			else 
+				begin
+				count_day <= count_day + 5'd1;   
+				end
+		   end
+end
+else*/
+begin
+//for 31days 
+		if(count_month<=7 && count_month%2 !=0 || count_month>7 && count_month%2 == 0)
+			begin
+			if (count_day == 5'd31) 
+				begin
+				count_day <= 5'd1;   
+				if (count_month == 4'd12) 
+					begin
+					count_month <= 5'd1;   
+					count_year <= count_year + 7'd1;   
+					end 
+				else 
+					begin
+					count_month <= count_month + 5'd1;   
+					end
+            end
+			else 
+				begin
+				count_day <= count_day + 5'd1;   
+				end
+		   end
+
+//for feb 28 days
+
+else if(count_month==2)
+			begin
+			if (count_day == 5'd28) 
+				begin
+				count_day <= 5'd1;   
+				if (count_month == 4'd12) 
+					begin
+					count_month <= 5'd1;   
+					count_year <= count_year + 7'd1;   
+					end 
+				else 
+					begin
+					count_month <= count_month + 5'd1;   
+					end
+            end
+			else 
+				begin
+				count_day <= count_day + 5'd1;   
+				end
+		   end
+		
+//for 30days
+else if(count_month<=7 && count_month%2 ==0 || count_month>7 && count_month%2 != 0)
+			begin
+			if (count_day == 5'd30) 
+				begin
+				count_day <= 5'd1;   
+				if (count_month == 4'd12) 
+					begin
+					count_month <= 5'd1;   
+					count_year <= count_year + 7'd1;   
+					end 
+				else 
+					begin
+					count_month <= count_month + 5'd1;  
+					end
+            end
+			else 
+				begin
+				count_day <= count_day + 5'd1;  
+				end
+		   end				
+end
+end
+end
+  assign year = count_year;
+  assign month = count_month;
+  assign day = count_day;
+
+endmodule
+
---
+++ tb.v
@@ -0,0 +1,59 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   21:45:18 07/14/2023
+// Design Name:   DigitalCalendar
+// Module Name:   E:/Digital_calender_MP/tb.v
+// Project Name:  Digital_calender_MP
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: DigitalCalendar
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module DigitalCalendar_TB;
+
+  reg clk;
+  reg reset;
+  wire [6:0] year;
+  wire [4:0] month;
+  wire [4:0] day;
+
+  DigitalCalendar dut (
+    .clk(clk),
+    .reset(reset),
+    .year(year),
+    .month(month),
+    .day(day)
+  );
+
+    initial clk= 0;
+    always #5 clk = ~clk;  
+
+    initial begin
+        reset = 1;
+        #10;
+        reset = 0; 
+		  #100;
+		  clk = 1;
+		  #10;
+		  reset=1;
+		  #20;
+		  reset=0;
+		  #20;
+    end
+
+endmodule
+
+
